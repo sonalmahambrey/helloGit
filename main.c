b77669129c43148abf1ad0569d51dff397a22ff7
sonalmahambrey <sonal_mahambrey@persistent.co.in>
2015-01-21 11:03:11 +0530

    adding changes

--- main.c
+++ main.c
@@ -2,6 +2,6 @@
 int main(int argc, char*argv[])
 {
 printf("hello this is my first git project\n");
-printf("clonning success. updating the changes to repo\n");
+printf("clonning success. updating the changes to repository\n");
 return 0;
 }
